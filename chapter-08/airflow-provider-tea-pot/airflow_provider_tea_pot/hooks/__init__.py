449f82ce8f478f8dc75ada5aebb19f4b21b9a556
Dylan Storey <dstorey@gmail.com>
2023-12-03 11:30:01 -0500

    editable installs in the docker files cause issues

--- chapter-08/airflow-provider-tea-pot/airflow_provider_tea_pot/hooks/__init__.py
+++ chapter-08/airflow-provider-tea-pot/airflow_provider_tea_pot/hooks/__init__.py
@@ -15,16 +15,10 @@ logger = logging.getLogger("airflow")
 
 class TeaPotHook(BaseHook):
     """
-    Sample Hook that interacts with an HTTP endpoint the Python requests library.
+    A simple hook that allows us to interact with our smart Tea Pot.
 
-    :param method: the API method to be called
-    :type method: str
-    :param sample_conn_id: connection that has the base API url i.e https://www.google.com/
-        and optional authentication credentials. Default headers can also be specified in
-        the Extra field in json format.
+    :param sample_conn_id: connection that has the base url for the tea pot 
     :type sample_conn_id: str
-    :param auth_type: The auth type for the service
-    :type auth_type: AuthBase of python requests lib
     """
 
     conn_name_attr = "tea_pot_conn_id"
@@ -40,37 +34,30 @@ class TeaPotHook(BaseHook):
         super().__init__()
         self.tea_pot_conn_id = tea_pot_conn_id
 
-
-
-    @staticmethod
-    def get_connection_form_widgets() -> dict[str, typing.Any]:
-        """Returns connection widgets to add to connection form"""
-        return {
-            }
-
-    @staticmethod
-    def get_ui_field_behaviour() -> dict:
-        """Returns custom field behaviour"""
-        return {
-            }
-
-
     def get_conn(self) -> typing.Any:
         """
         A method for getting a connection to an external service given your connection information.
         """
-        self.get_connection(self.sample_conn_id)
-        raise NotImplementedError
+        conn = self.get_connection(self.sample_conn_id)
+        return conn
 
+    # @staticmethod
+    # def get_connection_form_widgets() -> dict[str, typing.Any]:
+    #     """Returns connection widgets to add to connection form"""
+    #     return {
+    #         }
+
+    # @staticmethod
+    # def get_ui_field_behaviour() -> dict:
+    #     """Returns custom field behaviour"""
+    #     return {
+    #         }
 
 
     def test_connection(self) -> typing.Tuple[bool, str]:
         """Test a connection"""
-
-        raise NotImplementedError
-
         x = self.get_conn()
-        alive = x.test_alive()
+        alive = True 
         if alive:
             return [True, "Alive"]
         return [False, "Not Alive"]
