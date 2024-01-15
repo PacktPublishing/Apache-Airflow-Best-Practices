bdaef93f0b86d0f89a47b529b71f6dddbdc52087
Dylan Storey <dstorey@gmail.com>
2024-01-15 14:03:03 -0500

    stopping point

--- chapter-10/dags/example_dag.py
+++ chapter-10/dags/example_dag.py
@@ -1,29 +1,44 @@
 
 from datetime import datetime
 
+
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import BranchPythonOperator, PythonOperator
-from example_dag import _choosing_best_model, _train_model
 
 from airflow import DAG
 
-# Generate 3 model training scenarios
-train_model_tasks = [
-    PythonOperator(
-        task_id=f"train_model_{model_id}",
-        python_callable=_train_model,
-        op_kwargs={"model": model_id},
-    )
-    for model_id in ["A", "B", "C"]
-]
+
+def _setup():
+    pass
+
+def _teardown():
+    pass
 
 with DAG(
     "my_dag", start_date=datetime(2021, 1, 1), schedule_interval="@daily", catchup=False
 ) as dag:
-    choosing_best_model = BranchPythonOperator(
-        task_id="choosing_best_model", python_callable=_choosing_best_model
+    
+    setup_environment = PythonOperator(
+        task_id="setup_environment",
+        python_callable=_setup,
     )
-    accurate = BashOperator(task_id="accurate", bash_command="echo 'accurate'")
-    inaccurate = BashOperator(task_id="inaccurate", bash_command=" echo 'inaccurate'")
 
-train_model_tasks >> choosing_best_model >> [accurate, inaccurate]
+    def worker_task_func():
+        return "Doing some work!"
+
+    worker_task_obj = PythonOperator(
+        task_id="worker_task",
+        python_callable=worker_task_func,
+    )
+
+    worker_task_obj_2 = PythonOperator(
+        task_id="worker_task_2",
+        python_callable=worker_task_func,
+    )
+
+    teardown_environment = PythonOperator(
+        task_id="teardown_environment",
+        python_callable=_teardown,
+    )
+
+    setup_environment.as_setup() >> [worker_task_obj, worker_task_obj_2] >> teardown_environment.as_teardown()
