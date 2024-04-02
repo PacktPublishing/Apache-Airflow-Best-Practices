2b6bfe5e50508846b2171fe78f7dddfeaa916707
Dylan Storey <dylanbstorey@users.noreply.github.com>
2024-04-02 15:51:12 -0400

    Chapter 13 (#8)
    * chapter 13 custom RBAC in
    
    * first draft in
    
    * updated permissions

---
+++ chapter-13/dags/user_2/example_dag.py
@@ -0,0 +1,25 @@
+
+from datetime import datetime
+
+from airflow.operators.python import  PythonOperator
+
+
+from airflow import DAG
+
+
+def _hello():
+    pass
+
+
+default_args = dict(
+    queue ="user_2"
+)
+
+with DAG(
+    "user_2_dag_1", start_date=datetime(2021, 1, 1), schedule_interval="@daily", catchup=False, default_args = default_args
+) as dag:
+
+    add_users = PythonOperator(task_id="add_users", python_callable=_hello)
+    
+
+
