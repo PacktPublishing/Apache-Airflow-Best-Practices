6de4a936c4fff324c9f50ed2e0c05d0b0217b806
Dylan Storey <dylan.storey@dylan.storey>
2024-08-21 11:48:23 -0400

    adding chapter-05 conentent in

---
+++ chapter-05/space_imags/dags/etl_asod.py
@@ -0,0 +1,54 @@
+import json
+import pathlib
+
+import airflow
+import requests
+import requests.exceptions as request_exceptions
+from datetime import date
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import task
+from datetime import datetime, timedelta 
+
+dag_owner = 'Kendrick'
+
+def _get_pictures():
+    # Ensure directory exists
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
+    api_key = 'INSERT API KEY'
+    url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+    response = requests.get(url).json()
+    today_image = response['hdurl']
+    #download astronomy image of the day
+    with open(f'todays_image_{date.today()}.png', 'wb') as f:
+        f.write(requests.get(today_image).content)   
+
+default_args = {'owner': dag_owner,
+        'depends_on_past': False,
+        'retries': 2,
+        'retry_delay': timedelta(minutes=5)
+        }
+
+with DAG(dag_id='download_ASOD_image',
+        default_args=default_args,
+        description='download and notify ',
+        start_date =airflow.utils.dates.days_ago(0),
+        schedule_interval='@daily',
+        catchup=True,
+        tags=['None']
+):
+ 
+        get_pictures = PythonOperator(
+                task_id="get_pictures",
+                python_callable=_get_pictures,
+        )
+ 
+        notify = BashOperator(
+                task_id="notify",
+                bash_command='echo "Images for $today_date have been added!"',
+
+        )
+ 
+get_pictures >> notify
+ 
